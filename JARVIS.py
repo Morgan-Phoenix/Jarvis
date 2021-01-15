4b7c221660025fae0cfd0383fa96d87564e0d1bc
Morgan-Phoenix <73711602+Morgan-Phoenix@users.noreply.github.com>
2021-01-15 23:11:15 +0530

    Added Encryption
    Added text and file encryption. if the program does not have your answer it will search for it. A little more intraction

--- JARVIS.py
+++ JARVIS.py
@@ -3,6 +3,8 @@ import os
 import smtplib
 import time
 import webbrowser
+import base64
+import binhex
 
 import pyttsx3  # pip install pyttsx3
 import speech_recognition as sr  # pip install speechRecognition
@@ -33,8 +35,6 @@ def wishMe():
     speak("I am Jarvis Sir. Please tell me how may I help you")       
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
-
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -48,8 +48,7 @@ def takeCommand():
 
     except Exception as e:
         del e
-        print("This is not in my program sir")
-        speak("This is not in my program sir")  
+        speak("This Is Not In My Program Sir")
         return "None"
     return query
 
@@ -83,17 +82,31 @@ def calculate(first_number,operation,second_number):
                 del e
         if operation == 'divided':
             try:
-                speak(first_number / second_number)
                 print(first_number / second_number)
+                speak(first_number / second_number)
                 print("Remainder Is ",first_number % second_number )
-                speak("Remainder Is ",first_number % second_number )
+                speak("Remainder Is ")
+                speak(first_number % second_number)
             except Exception as e:
                 del e
     except Exception as e:
         del e
         speak("something went wrong")
 
+def encrypter(Message):
+    b =  bytes(Message.encode())
+    c = base64.urlsafe_b64encode(b)
+    print(c)
 
+def decrypter(Message):
+    b = bytes(Message.encode())
+    c = base64.urlsafe_b64decode(b)
+    print(c)
+    speak("After Decrypting The Sentence Is")
+    speak(c)
+
+def encrypt_file(Infile_path : str,outfile_path : str):
+    binhex.binhex(Infile_path,outfile_path)
 if __name__ == "__main__":
     wishMe()
     while True:
@@ -101,12 +114,24 @@ if __name__ == "__main__":
 
         # Logic for executing tasks based on query
         if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=20)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
+            speak("Do You Want To Type Or Do You Want To Speak Your Question")
+            search = takeCommand()
+            if 'type' in search:
+                speak("Type Your Question")
+                tsearch = input("Here ")
+                speak('Searching Wikipedia...')
+                tsearch = tsearch.replace("wikipedia", "")
+                results = wikipedia.summary(tsearch, sentences=20)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
+            if 'speak' in search:
+                speak('Searching Wikipedia...')
+                query = query.replace("wikipedia", "")
+                results = wikipedia.summary(query, sentences=20)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
 
         elif 'open youtube' in query:
             speak("Opening youtube")
@@ -165,8 +190,8 @@ if __name__ == "__main__":
             webbrowser.BackgroundBrowser("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
 
         elif 'open this url' in query:
-            speak("Please tell the URL")
-            ourl = takeCommand()
+            speak("Please Write the URL")
+            ourl = input("Here  ") 
             speak("Opening")
             chrome_path="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
             webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path))
@@ -176,7 +201,7 @@ if __name__ == "__main__":
 
 
         elif 'play music' in query:
-            music_dir = 'C:\\Non Critical\\songs\\Favorite Songs2'
+            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
             songs = os.listdir(music_dir)
             print(songs)    
             os.startfile(os.path.join(music_dir, songs[0]))
@@ -184,16 +209,10 @@ if __name__ == "__main__":
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
-           
+
         elif 'open code' in query:
             codePath = "C:\\Users\\hp\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code\\Visual Studio Code.lnk"
             os.startfile(codePath)
-        
-        elif 'what can you do' in query:
-            speak("I can send emails(if configured), i can open many things in chrome, play music, etcetera")
-        
-        elif 'hello' in query:
-            speak("Hello sir, how can i help you")
     
         elif 'calculator' in query:
             try:
@@ -220,18 +239,122 @@ if __name__ == "__main__":
                     speak("Time is up")
                     speak("Time is up")
                     speak("Time is up")
+        elif "encrypt text" in query:
+            speak("Please Write The Password Sir")
+            passw = input("Here ")
+            if passw == "1234567890@morgan":
+                speak("Access Granted")
+                time.sleep(1.1)
+                speak("What Is The Message Sir")
+                message = takeCommand()
+                encrypter(message)
+            else:
+                speak("Wrong Password")
+                speak("Access Not Granted")
+
+        elif "decrypt text" in query:
+            speak("Please Write The Password Sir")
+            passwo = input("Here ")
+            if passwo == "1234567890@morgan-phoenix":
+                speak("Access Granted")
+                time.sleep(1.1)
+                speak("Sir, Please Write The Encrypted Text ")
+                text = input("Here  ")
+                decrypter(text)
+            else:
+                speak("Access Not Granted")
 
         elif 'send a email' in query:
-            try:
-                speak("Whom to send")
-                to = takeCommand()   
-                speak("What should I say?")
-                content = takeCommand()
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry sir. I am not able to send this email")
+            epass = "emailunlock@3540P"
+            speak("Please Write The Password")
+            epass_t = input("Here ")
+            if epass_t == epass:
+                speak("Access Granted")
+                time.sleep(1.1)
+                try:
+                    speak("Whom to send")
+                    to = takeCommand()   
+                    speak("What should I say?")
+                    content = takeCommand()
+                    sendEmail(to, content)
+                    speak("Email has been sent!")
+                except Exception as e:
+                    print(e)
+                    speak("Sorry sir. I am not able to send this email")
+            else:
+                speak("Access Not Granted")
+        
+        elif 'encrypt file' in query:
+            filepass = "fileencrypto368OP0"
+            speak("Please Write the password")
+            filepass_test = input("Here ")
+            if filepass == filepass_test:
+                try:
+                    speak("Access Granted")
+                    time.sleep(1.1)
+                    speak("Please write the file path that you want to encrypt")
+                    infile = input("Here ")
+                    speak("Please write file path in which you want to collect the output")
+                    outfile = input("Here ")
+                    encrypt_file(infile,outfile)
+                except Exception as e:
+                    del e
+                    speak("Something Went Wrong")
+            else:
+                speak("Access Not Granted")
+        
         elif 'jarvis exit' in query:
-            speak("Shuting down")
-            exit(code="Shuting down") 
+            exit(code="Shuting Down")
+            speak("Shut ing Down") 
+# Talking to the User 
+        elif 'what can you do' in query:
+            speak("I can send emails(if configured), i can open many things in chrome, play music, do some encrypting decrypting, etcetera")
+        
+        elif 'hello' in query:
+            speak("Hello sir, how can i help you")
+        
+        elif 'say sorry' in query:
+            speak("Sorry Sir If I Did Something Wrong") 
+        
+        elif 'how are you' in query:
+            speak("i am fine sir, What about you?")
+
+        elif 'i am fine' in query:
+            speak("that is nice, please tell me how may i help you?")
+
+        elif 'what is in your program' in query:
+            speak("I am in devloping stage please forgive me if i missed something.")
+
+        elif 'thank you'in query:
+            speak("Welcom Sir")
+
+        else:
+                speak("How Much Details You Want? Less Details, More Details, All details")
+                details = takeCommand()
+                if "less" in details:
+                    try:
+                        speak('searching...')
+                        query = query.replace("wikipedia", "")
+                        results = wikipedia.summary(query, sentences=5)
+                        speak(results)
+                    except Exception as e:
+                        del e
+                        speak("Sorry Sir, I Did not found Anything")
+                if "more" in details:
+                    try:
+                        speak('searching...')
+                        query = query.replace("wikipedia", "")
+                        results = wikipedia.summary(query, sentences=10)
+                        speak(results)
+                    except Exception as e:
+                        del e
+                        speak("Sorry Sir, I Did not found Anything")
+                if "all" in details:
+                    try:
+                        speak('searching...')
+                        query = query.replace("wikipedia", "")
+                        results = wikipedia.summary(query, sentences=100)
+                        speak(results)
+                    except Exception as e:
+                        del e
+                        speak("Sorry Sir, I Did not found Anything")
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Jarvis
+Requirements:
+    Using Python 3.9.1
+    4 downloaded Modules
+    *pyttsx3
+    *speech_recognition
+    *Wikipedia
+    *pyaudio(without importing)
+
+Used Modules:
+    *os
+    *base64
+    *time
+    *webbrowser
+    *datetime
+    *smtplib
+    *binhex
+    
+Passwords:
+    encrypt = 1234567890@morgan
+    decrypt = 1234567890@morgan-phoenix
+    email = emailunlock@3540P
+    file encrypter = fileencrypto368OP0
+
+Why Passwords Are Required??
+    if the program's output lead to a error, the program is not able to encrypt files and send emails,
+    moreover nobody can send emails and encrypt files if he/she does know the password.
